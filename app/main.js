55f4354f1fd20b96c04bd4cc1908d0f5809393b1
Marta Herradon <marta_h.93@hotmail.com>
2021-05-30 18:27:08 +0200

    first commit

--- app/main.js
+++ app/main.js
@@ -1,111 +1,70 @@
-/** Crear / conectar bbdd */
-let database = new PouchDB('temperaturaMarte');
+
 
 window.onload = function(){
 
-	function mostrar() {
-		document.getElementById("sidebar").style.width = "300px";
-		document.getElementById("contenido").style.marginLeft = "300px";
-		document.getElementById("abrir").style.display = "none";
-		document.getElementById("cerrar").style.display = "block";
-	}
-	
-	function ocultar() {
-		document.getElementById("sidebar").style.width = "0";
-		document.getElementById("contenido").style.marginLeft = "0";
-		document.getElementById("abrir").style.display = "block";
-		document.getElementById("cerrar").style.display = "none";
-	}
-
-
-	fetch(`https://mars-weather-rems.netlify.app/rems.json`)
+	function fetchPokemon(id){
+	fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
 		.then(data => {
 			return data.json();
 		})
 		.then(data => {
-			let cajaTemperatura = document.querySelector(".container_temperaturas");
-			data.weather_report.magnitudes.forEach(function () {
-				let temp2 = `
-				<div class="temperaturaPrevision">
-					<h3 class="icono" ><i class="fas fa-temperature-high"></i></h3>	
-					<h4>${data.weather_report.magnitudes[0].max_temp}max.</h4>
-					<h4>${data.weather_report.magnitudes[0].min_temp}min.</h4>  
-				</div>
-				<span class="borde"></span>
-				<div class="temperatura_aire">
-					<h3 class="icono"><i class="fas fa-clock"></i></h3>
-					<h4>${data.weather_report.magnitudes[0].sunrise} salida</h4>
-					<h4>${data.weather_report.magnitudes[0].sunset} puesta</h4>
-				</div>
-				<span class="borde"></span>
-				<div class="temperatura_suelo">
-					<h3 class="icono"><i class="fas fa-mountain"></i></h3>
-					<h4>${data.weather_report.magnitudes[0].max_gts_temp}max.</h4>
-					<h4>${data.weather_report.magnitudes[0].min_gts_temp}min.</h4>
-				</div`;
-					cajaTemperatura.innerHTML += temp2;
-			});
-			console.log(data.weather_report.terrestrial_date[0]);
-
-			let cajaHora = document.querySelector(".reloj");
-			data.weather_report.terrestrial_date.forEach(function () {
-				let hora = `
-				<div class="marte_hour">
-				<div class="reloj">
-				<p id="horas" class="horas">${data.weather_report.terrestrial_date[0]}</p>
-					</div>
-
-				</div>
-				</div>`;
-				cajaHora.innerHTML += hora;
-
-				let cajaSol = document.querySelector("#sol");
-				data.weather_report.sol.forEach(function () {
-					let sumMars = `
-						<h2>Sol</h2>
-						<div id="sol">${data.weather_report.sol[0]}</div>
-						`;
-					cajaSol.innerHTML += sumMars;
-
-				});
-			});
-
-				
+			console.log(data);
+			let cajaPokemon = document.querySelector(".pokemon-container");
+			let htmlPokemon = `
+					<article class="card">
+						
+						<div class="card-body">
+						<img src="${data.sprites.front_default}"  class="card-header">
+							<img src="" class="card-body-img">
+							<h1 class="card-body-title">
+								<p>${capitalizeFirstLetter(data.name)}</p>
+								<span class="borde"></span>
+							</h1>
+							<p class="card-body-text"></p>
+						</div>
+						<div class="card-footer">
+							<div class="habilidades">
+								<h3>Peso ${data.weight} kg.</h3>
+								<p>Ataque</p>
+							</div>
+							<div class="habilidades">
+								<h3></h3>
+								<p>Naturaleza</p>
+							</div>
+							<div class="habilidades">
+								<h3></h3>
+								<p>Nivel</p>
+							</div>
+						</div>
+					</article>`;
+			cajaPokemon.innerHTML += htmlPokemon;
+			
 		});
-		
-		let temp;
+			
+    };
 
-		
 
-		/** Pintar la lista de usuarios */
-		//renderTemperatura();
+	function capitalizeFirstLetter(texto) {
+		if (texto) {
+			return texto.charAt(0).toUpperCase() + texto.slice(1);
+		} else {
+			return "";
+		}
+	  }
 
-		/** Escuechar eventos de los botones */
-		let btnAdd = document.querySelector("#add");
-		btnAdd.addEventListener("click", addData, false);
+	 function fetchPokemons(number){
+	 	for (let i = 1; i<= number; i++) {
+	 		fetchPokemon(i);
+	 	}
+	 };
 
-		/** Función para añadir usuarios */
-		function addData() {
-			if (confirm("¿Quiere guardar los datos actuales de Marte?")) {
-				let cajaTemperaturas = document.querySelector(".container_temperaturas");
+	 fetchPokemons(12);
 	
-				if (cajaTemperaturas.innerHTML != "") {
-
-					var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
-					var momentoActual  = new Date();
-
-					// Añadir registro a la BBDD
-					let objetoQueVamosAGuardarEnBD = {
-						"_id": `${momentoActual.toLocaleString()}`,
-						"fecha": `${momentoActual.toLocaleString()}`,
-						"cajaInformacion": cajaTemperaturas.innerHTML,
-					 };
-					database.put(objetoQueVamosAGuardarEnBD);
-				};
-			}
-		};
-
-
-		
+	// function createPokemonCard(pokemon){
+	// 	const card = document.querySelector('.card');
+	// 	card.classList.add('pokemon-block');
+	// 	const spriteContainer
+	// }
 
 };
+
